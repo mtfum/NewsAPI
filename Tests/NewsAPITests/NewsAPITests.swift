efc72955d2eb59b26c007fbc1b46dc6cf0ea2793
Kotaro Fukuo <haptaro.dev@gmail.com>
2021-07-15 22:30:43 -0700

    :art: improve NewsAPITests

--- Tests/NewsAPITests/NewsAPITests.swift
+++ Tests/NewsAPITests/NewsAPITests.swift
@@ -10,32 +10,27 @@ final class NewsAPITests: XCTestCase {
   func testGetTopHeadlines() async throws {
     do {
       let articles = try await client.getTopHeadlines()
-      assert(!articles.isEmpty)
+      XCTAssertTrue(!articles.isEmpty)
     } catch {
-      assertionFailure(String(describing: error))
+      XCTFail(String(describing: error))
     }
   }
 
   func testGetSources() async throws {
     do {
-      let articles = try await client.getSources()
-      assert(!articles.isEmpty)
+      let sources = try await client.getSources()
+      XCTAssertTrue(!sources.isEmpty)
     } catch {
-      if let e = (error as? ErrorResponse) {
-        print(e.code.rawValue, e.message)
-      }
-      assertionFailure(String(describing: error))
-
+      XCTFail(String(describing: error))
     }
   }
 
   func testSearch() async throws {
     do {
       let articles = try await client.search(query: "bitcoin")
-      assert(!articles.isEmpty)
+      XCTAssertTrue(!articles.isEmpty)
     } catch {
-      assertionFailure(String(describing: error))
+      XCTFail(String(describing: error))
     }
   }
-
 }
