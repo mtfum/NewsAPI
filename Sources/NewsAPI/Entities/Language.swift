cc385bc98173304acf10840d692411404449a87d
mtfum <f.yamanaka1127@gmail.com>
2021-07-15 20:32:11 -0700

    udpate Directory structure

--- Sources/NewsAPI/Entities/Language.swift
+++ Sources/NewsAPI/Entities/Language.swift
@@ -1,12 +1,3 @@
-//
-//  File.swift
-//  
-//
-//  Created by Fumiya Yamanaka on 2021/07/11.
-//
-
-import Foundation
-
 public enum Language: String, Decodable {
   case ar, de, en, es, fr, he, it, nl, no, pt, ru, se, ud, zh
 }
--- Sources/NewsAPI/Entities/NewsArticle.swift
+++ Sources/NewsAPI/Entities/NewsArticle.swift
@@ -7,7 +7,6 @@ public struct NewsArticle: Decodable {
   public let description: String?
   public let url: URL?
   public let urlToImage: URL?
-//  public let publishedAt: Date?
   public let content: String?
 }
 
@@ -22,7 +21,6 @@ extension NewsArticle: Equatable {
     && lhs.description == rhs.description
     && lhs.url == rhs.url
     && lhs.urlToImage == rhs.urlToImage
-//    && lhs.publishedAt == rhs.publishedAt
     && lhs.content == rhs.content
   }
 }
@@ -40,11 +38,6 @@ extension NewsArticle {
     description: "Three days after ransomware attackers hijacked a managed services platform, recovery efforts continued. The REvil group is reportedly asking for as much as $70 million in Bitcoin to unlock the more than 1 million devices infected.",
     url: URL(string: "https://www.theverge.com/2021/7/5/22564054/ransomware-revil-kaseya-coop"),
     urlToImage: URL(string: "https://cdn.vox-cdn.com/thumbor/nk-drxT0WYuHTTAQw6MhPgi4LK8=/0x146:2040x1214/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/8792137/acastro_170629_1777_0008_v2.jpg"),
-//    publishedAt: { () -> Date in
-//      let formatter = DateFormatter()
-//      formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-//      return formatter.date(from: "2021-07-05T19:45:10Z")!
-//    }(),
     content: "Filed under:\r\nThe supply chain attack has reached over a thousand organizations.\r\nIllustration by Alex Castro / The Verge\r\nThree days after ransomware attackers started the holiday weekend by comprom… [+3376 chars]"
   )
 }
